271a437d5531e9adba65ced59373432d7e42a3a0
Bhanuka Pawan Athukorala <bhanuka_pawan@yahoo.com>
2026-08-22 19:34:55 +0530

    Register app in google sign in

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.5.0" apply false  // Google Services plugin
 }
 
 android {
@@ -28,6 +29,15 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        // Import Firebase BoM
+        implementation(platform("com.google.firebase:firebase-bom:34.18.0"))
+
+        // When using the BoM, don't specify versions in Firebase dependencies
+        implementation("com.google.firebase:firebase-analytics")
+
+        // Add the dependencies for any other desired Firebase products
+        // https://firebase.google.com/docs/android/setup#available-libraries
     }
 
     buildTypes {
